1999dc856b8f02ae2811a47ce8ce0443927df04b
Игорь <vasyukovin@yandex.ru>
2021-07-19 14:40:48 +0300

    Исправил нейминг, добавил индексы и доп. таблицу для связи м-м

--- schema.sql
+++ schema.sql
@@ -5,32 +5,30 @@ CREATE DATABASE readme
 USE readme;
 
 CREATE TABLE users (
-       user_id INT AUTO_INCREMENT PRIMARY KEY,
+       id INT AUTO_INCREMENT PRIMARY KEY,
        registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-       email VARCHAR(128) UNIQUE,
+       email VARCHAR(255) UNIQUE,
        login VARCHAR(128) UNIQUE,
        password CHAR(64),
        avatar VARCHAR(255)
 );
 
-CREATE INDEX u_email ON users(email);
-
 CREATE TABLE content_types (
-       content_type_id INT AUTO_INCREMENT PRIMARY KEY,
-       type VARCHAR(128) UNIQUE,
-       class VARCHAR(128) UNIQUE
+       id INT AUTO_INCREMENT PRIMARY KEY,
+       type VARCHAR(32) UNIQUE,
+       image_class VARCHAR(128) UNIQUE
 );
 
 CREATE TABLE hashtags (
-       hashtag_id INT AUTO_INCREMENT PRIMARY KEY,
+       id INT AUTO_INCREMENT PRIMARY KEY,
        hashtag VARCHAR(128) UNIQUE
 );
 
 CREATE TABLE posts (
-       post_id INT AUTO_INCREMENT PRIMARY KEY,
+       id INT AUTO_INCREMENT PRIMARY KEY,
        publication_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
        title VARCHAR(255) NOT NULL,
-       description TEXT,
+       content TEXT,
        author VARCHAR(128),
        img VARCHAR(255),
        video VARCHAR(255),
@@ -38,46 +36,55 @@ CREATE TABLE posts (
        views INT,
        user_id INT,
        content_type_id INT,
-       hashtag_id INT,
-       FOREIGN KEY (user_id) REFERENCES users(user_id),
-       FOREIGN KEY (content_type_id) REFERENCES content_types(content_type_id),
-       FOREIGN KEY (hashtag_id) REFERENCES hashtags(hashtag_id)
+       FOREIGN KEY (user_id) REFERENCES users(id),
+       FOREIGN KEY (content_type_id) REFERENCES content_types(id)
+);
+
+CREATE INDEX title ON posts(title);
+CREATE INDEX views ON posts(views);
+
+CREATE TABLE posts_hashtags (
+       post_id INT NOT NULL,
+       hashtag_id INT NOT NULL,
+       FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
+       FOREIGN KEY (hashtag_id) REFERENCES hashtags(id) ON DELETE CASCADE,
+       PRIMARY KEY (post_id, hashtag_id)
 );
 
 CREATE TABLE comments (
-       comment_id INT AUTO_INCREMENT PRIMARY KEY,
+       id INT AUTO_INCREMENT PRIMARY KEY,
        publication_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
        content TEXT NOT NULL,
        author_id INT,
        post_id INT,
-       FOREIGN KEY (author_id) REFERENCES users(user_id),
-       FOREIGN KEY (post_id) REFERENCES posts(post_id)
+       FOREIGN KEY (author_id) REFERENCES users(id),
+       FOREIGN KEY (post_id) REFERENCES posts(id)
 );
 
 CREATE TABLE likes (
-       like_id INT AUTO_INCREMENT PRIMARY KEY,
+       id INT AUTO_INCREMENT PRIMARY KEY,
        user_id INT,
        post_id INT,
-       FOREIGN KEY (user_id) REFERENCES users(user_id),
-       FOREIGN KEY (post_id) REFERENCES posts(post_id)
+       FOREIGN KEY (user_id) REFERENCES users(id),
+       FOREIGN KEY (post_id) REFERENCES posts(id)
 );
 
 CREATE TABLE subscriptions (
-       subscription_id INT AUTO_INCREMENT PRIMARY KEY,
+       id INT AUTO_INCREMENT PRIMARY KEY,
        author_id INT,
        subscriber_id INT,
-       FOREIGN KEY (author_id) REFERENCES users(user_id),
-       FOREIGN KEY (subscriber_id) REFERENCES users(user_id)
+       FOREIGN KEY (author_id) REFERENCES users(id),
+       FOREIGN KEY (subscriber_id) REFERENCES users(id)
 );
 
 CREATE TABLE messages (
-       message_id INT AUTO_INCREMENT PRIMARY KEY,
+       id INT AUTO_INCREMENT PRIMARY KEY,
        publication_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
        content TEXT NOT NULL,
        sender_id INT,
        recipient_id INT,
-       FOREIGN KEY (sender_id) REFERENCES users(user_id),
-       FOREIGN KEY (recipient_id) REFERENCES users(user_id)
+       FOREIGN KEY (sender_id) REFERENCES users(id),
+       FOREIGN KEY (recipient_id) REFERENCES users(id)
 );
 
 
