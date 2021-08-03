618d8dd29a95ffa7cce09d1eec5b07acf1532d5b
Игорь <vasyukovin@yandex.ru>
2021-08-03 18:05:47 +0300

    Подправил форматирование и добавил лимит на количество постов

--- index.php
+++ index.php
@@ -4,14 +4,16 @@ require_once ('helpers.php');
 $con = mysqli_connect('localhost', 'root', '', 'readme');
 mysqli_set_charset($con, "utf8");
 $sqlContentTypes = '
-SELECT type, image_class
+SELECT type, title
   FROM content_types;
 ';
 $sqlPostList = '
 SELECT p.*, u.user_name, u.avatar, ct.image_class
-  FROM posts p JOIN users u ON p.user_id = u.id
-               JOIN content_types ct ON p.content_type_id = ct.id
- ORDER BY views DESC;
+  FROM posts p
+  JOIN users u ON p.user_id = u.id
+  JOIN content_types ct ON p.content_type_id = ct.id
+ ORDER BY views DESC
+ LIMIT 6;
 ';
 $resultContentTypes = mysqli_query($con, $sqlContentTypes);
 $resultPostList = mysqli_query($con, $sqlPostList);
