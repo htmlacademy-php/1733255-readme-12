e21017d4dfc30eeba0447cb74d73dfe45b382d22
Игорь <vasyukovin@yandex.ru>
2021-08-16 13:27:47 +0300

    Перешёл на "Подготовленные выражения"

--- index.php
+++ index.php
@@ -14,13 +14,17 @@ SELECT p.*, u.user_name, u.avatar, ct.type, ct.image_class
   FROM posts p
   JOIN users u ON p.user_id = u.id
   JOIN content_types ct ON p.content_type_id = ct.id
- WHERE IF ('$currentContentTypeId', p.content_type_id = '$currentContentTypeId', true)
+ WHERE IF (?, p.content_type_id = ?, true)
  ORDER BY views DESC
  LIMIT 6;
 ";
 
 $resultContentTypes = mysqli_query($con, $sqlContentTypes);
-$resultPostList = mysqli_query($con, $sqlPostList);
+
+$stmt = mysqli_prepare($con, $sqlPostList);
+mysqli_stmt_bind_param($stmt, 'ii', $currentContentTypeId, $currentContentTypeId);
+mysqli_stmt_execute($stmt);
+$resultPostList = mysqli_stmt_get_result($stmt);
 
 $rowContentTypes = mysqli_fetch_all($resultContentTypes, MYSQLI_ASSOC);
 $rowPostList = mysqli_fetch_all($resultPostList, MYSQLI_ASSOC);
