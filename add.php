64a11bb49644125afa9bc17a8f7f9b94cbd3304c
Игорь <vasyukovin@yandex.ru>
2021-09-13 11:37:19 +0300

    Перенёс валидацию в отдельные функции, вытащил sql запросы из цикла

--- add.php
+++ add.php
@@ -1,68 +1,69 @@
 <?php
 require_once ('helpers.php');
+require_once ('validation.php');
 
 // Соединение с БД для получения и отправки данных
 $con = mysqli_connect('localhost', 'root', '', 'readme');
 mysqli_set_charset($con, "utf8");
 
-// Показываем либо дефолтную форму, либо полученную через GET или POST
+// Получаем типы контента
+$sqlContentTypes = '
+SELECT type, title, id
+  FROM content_types;
+';
+$resultContentTypes = mysqli_query($con, $sqlContentTypes);
+$rowContentTypes = mysqli_fetch_all($resultContentTypes, MYSQLI_ASSOC);
+
+// Устанавливаем дефолтную форму
 $currentContentTypeId = '1';
 
-if (isset($_GET['contentId'])) {
+// Допустимые формы
+$allowedContentTypeIds = [];
+foreach ($rowContentTypes as $contentType) {
+    array_push($allowedContentTypeIds, $contentType['id']);
+}
+
+// Показываем либо дефолтную форму, либо полученную через GET или POST (с валидацией)
+if (isset($_GET['contentId']) && in_array($_GET['contentId'], $allowedContentTypeIds)) {
     $currentContentTypeId = ($_GET['contentId']);
-} elseif (isset($_POST['contentId'])) {
+} elseif (isset($_POST['contentId']) && in_array($_POST['contentId'], $allowedContentTypeIds)) {
     $currentContentTypeId = $_POST['contentId'];
 }
 
 $errors=[];
-$FILE_TYPES = ['image/png', 'image/jpeg', 'image/gif'];
+$rules = [
+    'heading' => function() {
+        return validateTitle('heading');
+    },
+    'content' => function() {
+        return validateContent('content');
+    },
+    'author' => function() {
+        return validateAuthor('author');
+    },
+    'url' => function() {
+        return validateUrl('url');
+    },
+];
+$fileTypes = ['image/png', 'image/jpeg', 'image/gif'];
 
-if (count($_POST)) {
+if (count($_POST) > 0) {
     // Формируем переменные для SQL запроса
-    $title = $_POST['heading'];
+    $title = $_POST['heading'] ?? '';
     $content = $_POST['content'] ?? '';
     $author = $_POST['author'] ?? '';
     $tags = [];
     $video = $_POST['contentType'] === 'video' ? $_POST['url'] : '';
     $reference = $_POST['contentType'] === 'link' ? $_POST['url'] : '';
     $userId = '2';
-    $contentTypeId = $_POST['contentId'];
+    $contentTypeId = $currentContentTypeId;
     $img = '';
 
     // Валидация полученных данных
     foreach ($_POST as $key => $value) {
-        // Проверяем заполнение необходимых полей
-        if (empty($value) && ($key === 'heading' || $key === 'content' || $key === 'author' || $key === 'url')) {
-            $errors[$key] = 'Поле не заполнено';
-        }
-
-        // Проверяем, что отправленная форма типа "ФОТО"
-        if (isset($_FILES['photo'])) {
-            if ($_FILES['photo']['error'] && $key === 'url' && empty($value)) { // Отсутствует ссылка и файл
-                $errors[$key] = 'Загрузите фотографию';
-            } elseif (!$_FILES['photo']['error'] && $key === 'url') { // Есть файл, игнорируем ссылку
-                unset($errors[$key]); // Убираем ошибку "Поле не заполнено"
-                if (!in_array($_FILES['photo']['type'], $FILE_TYPES)) { // Неверный формат файла
-                    $errors['url'] = 'Загрузите корректный тип файла';
-                } else if (!$errors) { // Загружаем файл при отсутствии ошибок (в т.ч) в других полях
-                    $fileName = $_FILES['photo']['name'];
-                    $filePath = __DIR__ . '/uploads/';
-                    $img = '../uploads/' . $fileName; // Для SQL запроса
-                    move_uploaded_file($_FILES['photo']['tmp_name'], $filePath . $fileName);
-                }
-            } elseif ($_FILES['photo']['error'] && $key === 'url' && filter_var($value, FILTER_VALIDATE_URL)) { // Файла нет, проверяем ссылку
-                if (!file_get_contents($value)) {
-                    $errors['url'] = 'Не удалось загрузить файл';
-                } else {
-                    $uploadedFile = file_get_contents($value);
-                    $fileName = explode('.', parse_url(basename($value))['path'])[0]; // Получаем имя файла без расширения
-                    $fileExtension = explode('/', get_headers($value, 1)['Content-Type'])[1]; // Получаем расширение в независимости, было оно в URL или нет
-                    $filePath = __DIR__ . '/uploads/';
-                    $fullPath = $filePath . $fileName . '.' . $fileExtension;
-                    $img = '../uploads/' . $fileName . '.' . $fileExtension; // Для SQL запроса
-                    file_put_contents($fullPath, $uploadedFile);
-                }
-            }
+        if (isset($rules[$key])) {
+            $rule = $rules[$key];
+            $errors[$key] = $rule();
         }
 
         //Проверяем наличие тегов
@@ -78,71 +79,114 @@ if (count($_POST)) {
                 $tags = $letters;
             }
         }
-
-        // Проверяем ссылки всех типов форм на корректный формат
-        if ($key === 'url' && !empty($value) && !filter_var($value, FILTER_VALIDATE_URL)) {
-            $errors[$key] = 'Введите корректную ссылку';
-        }
-
-        // Проверяем ссылку на YouTube из типа "ВИДЕО"
-        if ($key === 'url' && $_POST['contentType'] === 'video') {
-            if (!is_bool(checkYoutubeUrl($value))) {
-                $errors[$key] = checkYoutubeUrl($value);
-            }
-        }
     }
 
-    // Отправляем данные в БД при отсутствии ошибок
-    if (!count($errors)) {
+    // Удаляем ключ с отсутствующим значением
+    $errors = array_filter($errors);
+
+    // Отправляем данные в БД при отсутствии ошибок + загружаем фото
+    if (count($errors) === 0) {
+        // Загружаем фото
+        if (isset($_FILES['photo'])) {
+            if ($_FILES['photo']['name']) {
+                $fileName = $_FILES['photo']['name'];
+                $filePath = __DIR__ . '/uploads/';
+                $img = '../uploads/' . $fileName; // Для SQL запроса
+                move_uploaded_file($_FILES['photo']['tmp_name'], $filePath . $fileName);
+            } else {
+                $uploadedFile = file_get_contents($_POST['url']);
+                $fileName = explode('.', parse_url(basename($_POST['url']))['path'])[0]; // Получаем имя файла без расширения
+                $fileExtension = explode('/', get_headers($_POST['url'], 1)['Content-Type'])[1]; // Получаем расширение в независимости, было оно в URL или нет
+                $filePath = __DIR__ . '/uploads/';
+                $fullPath = $filePath . $fileName . '.' . $fileExtension;
+                $img = '../uploads/' . $fileName . '.' . $fileExtension; // Для SQL запроса
+                file_put_contents($fullPath, $uploadedFile);
+            }
+        }
+
         // Добавляем пост в БД
         $sqlNewPost = '
         INSERT INTO posts (title, content, author, img, video, reference, user_id, content_type_id)
         VALUES (?, ?, ?, ?, ?, ?, ?, ?);
         ';
-        $stmt = mysqli_prepare($con, $sqlNewPost);
-        mysqli_stmt_bind_param($stmt, 'ssssssii', $title, $content, $author, $img, $video, $reference, $userId, $contentTypeId);
-        mysqli_stmt_execute($stmt);
+
+        mysqli_stmt_execute(dbGetPrepareStmt($con, $sqlNewPost, [$title, $content, $author, $img, $video, $reference, $userId, $contentTypeId]));
 
         $newPostId = mysqli_insert_id($con); // Сохраняем ID нового поста
 
         // Проверяем наличие тегов
-        if (count($tags)) {
-            $sqlOldTags = '
+        if (count($tags) > 0) {
+            $sqlTags = '
             SELECT id, hashtag
               FROM hashtags
-             WHERE hashtag = ?;
             ';
-            $sqlNewTags = '
-            INSERT INTO hashtags (hashtag)
-            VALUES (?);
-            ';
-            $sqlTagsPostsCon = '
-            INSERT INTO posts_hashtags (post_id, hashtag_id)
-            VALUES (?, ?);
-            ';
-            foreach ($tags as $tag) {
-                // Проверяем наличие тега в базе
-                $oldTagsStmt = mysqli_prepare($con, $sqlOldTags);
-                mysqli_stmt_bind_param($oldTagsStmt, 's', $tag);
-                mysqli_stmt_execute($oldTagsStmt);
-                $resultOldTags = mysqli_stmt_get_result($oldTagsStmt);
-                $oldTag = mysqli_fetch_all($resultOldTags);
+            $resultOldTags = mysqli_query($con, $sqlTags);
+            $rowOldTags = mysqli_fetch_all($resultOldTags, MYSQLI_ASSOC);
 
-                if (!empty($oldTag)) { // Если тег уже есть в БД, связываем его с постом
-                    $tagId = $oldTag[0][0];
-                } else {
-                    // Сохраняем новые теги
-                    $newTagsStmt = mysqli_prepare($con, $sqlNewTags);
-                    mysqli_stmt_bind_param($newTagsStmt, 's', $tag);
-                    mysqli_stmt_execute($newTagsStmt);
+            $tagIds = []; // массив ID тегов для добавления в таблицу связей тега и поста
 
-                    $tagId = mysqli_insert_id($con); // Сохраняем ID нового тега
+            // Если тег уже в базе, сохраняем его ID и убираем из массива тегов поста
+            foreach ($rowOldTags as $oldTag) {
+                foreach ($tags as $tagIndex => $tagValue) {
+                    if ($oldTag['hashtag'] === $tagValue) {
+                        array_push($tagIds, intval($oldTag['id']));
+                        unset($tags[$tagIndex]);
+                    }
+                }
+            }
+
+            // Оставшиеся в массиве теги добавляем в таблицу тегов
+            if (count($tags) > 0) {
+                $insertTagValue = '(?)';
+                $insertTagValuesSum = [];
+
+                $selectTagValue = '?';
+                $selectTagValuesSum = [];
+
+                foreach ($tags as $tag) {
+                    array_push($insertTagValuesSum, $insertTagValue);
+                    array_push($selectTagValuesSum, $selectTagValue);
                 }
 
-                // Сохраняем новые связи тегов
-                $newTagsPostsConStmt = mysqli_prepare($con, $sqlTagsPostsCon);
-                mysqli_stmt_bind_param($newTagsPostsConStmt, 'ii', $newPostId, $tagId);
-                mysqli_stmt_execute($newTagsPostsConStmt);
+                $sqlCreateTags = '
+                INSERT INTO hashtags (hashtag)
+                VALUES ' . implode(',', $insertTagValuesSum);
+                mysqli_stmt_execute(dbGetPrepareStmt($con, $sqlCreateTags, [...$tags]));
+
+                // Получаем добавленные теги для определения их ID
+                $sqlNewTags = '
+                SELECT id, hashtag
+                  FROM hashtags
+                 WHERE hashtag IN (' . implode(',', $selectTagValuesSum) . ')';
+                $stmt = dbGetPrepareStmt($con, $sqlNewTags, [...$tags]);
+                mysqli_stmt_execute($stmt);
+                $resultNewTags = mysqli_stmt_get_result($stmt);
+                $rowUpdatedTags = mysqli_fetch_all($resultNewTags, MYSQLI_ASSOC);
+
+                // Сохраняем ID добавленных в базу тегов
+                foreach ($rowUpdatedTags as $updatedTag) {
+                    array_push($tagIds, $updatedTag['id']);
+                }
+
+                // Подготавливаем данные для отправки в таблицу связи ID поста и ID тега
+                $insertTagsPostsConValue = '(? , ?)';
+                $insertTagsPostsConValuesSum = [];
+
+                foreach ($tagIds as $tagId) {
+                    array_push($insertTagsPostsConValuesSum, $insertTagsPostsConValue);
+                }
+
+                // Добавляем ID поста к каждому ID хэштега
+                $postTagsIds = [];
+                foreach ($tagIds as $tagId) {
+                    array_push($postTagsIds, $newPostId);
+                    array_push($postTagsIds, $tagId);
+                }
+
+                $sqlTagsPostsCon = '
+                INSERT INTO posts_hashtags (post_id, hashtag_id)
+                VALUES ' . implode(',', $insertTagsPostsConValuesSum);
+                mysqli_stmt_execute(dbGetPrepareStmt($con, $sqlTagsPostsCon, [...$postTagsIds]));
             }
         }
 
@@ -150,14 +194,6 @@ if (count($_POST)) {
         header('Location: post.php?postId=' . $newPostId);
     }
 }
-
-// Получаем типы контента
-$sqlContentTypes = '
-SELECT type, title, id
-  FROM content_types;
-';
-$resultContentTypes = mysqli_query($con, $sqlContentTypes);
-$rowContentTypes = mysqli_fetch_all($resultContentTypes, MYSQLI_ASSOC);
 
 // Формируем страницу
 $userName = 'Игорь';
