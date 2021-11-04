b4da50c8c4d7a749de8b9e6c4b5cb6d51c27dd91
Игорь <vasyukovin@yandex.ru>
2021-11-04 14:56:29 +0300

    Вынес проверку сессии в функцию

--- feed.php
+++ feed.php
@@ -1,11 +1,7 @@
 <?php
 require_once ('helpers.php');
 
-session_start();
-
-if ( empty($_SESSION) ) {
-    header('Location: index.php');
-}
+checkSession();
 
 $mainContent = include_template('feed.php');
 $layoutContent = include_template('layout.php', prepareLayoutData($mainContent, 'Лента'));
